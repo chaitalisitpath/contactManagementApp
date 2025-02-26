a3f4eff82c0aa4db2c47e509ee97586fee9ed21b
Chaitali <chaitalis.itpath@gmail.com>
2025-02-26 11:54:30 +0530

    button added

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -25,7 +25,10 @@ function Contact() {
               <label for="msg" className="peer-focus:font-medium absolute text-md text-gray-700 dark:text-gray-500 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Message</label>
             </div>
           </div>
-          {/* <button type="submit" class="text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-green-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button> */}
+          <div className="flex justify-center mt-10">
+            <button className="pr-4 rounded-xl pl-4 pt-2 pb-2 text-white bg-green-500">Submit</button>
+
+          </div>
         </form>
 
       </div>
